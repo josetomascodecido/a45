8fe49b6bffe27c5b5ed6554de4fd36f7f6c1dfce
Jose Tomas <josetomascodecido@gmail.com>
2018-10-10 15:18:51 -0300

    exercise 6

---
+++ script.js
@@ -0,0 +1,15 @@
+// Crear una función llamada sortList() la cual debe ser ejecutada al presionar el botón “Clasificar Arreglo”.
+//
+// La función debe iterar el arreglo:
+//
+// Si el elemento del array es un número impar debe ser agregado a la lista ‘Impares’.
+// Si el elemento es un número par debe ser agregado a la lista ‘Pares’.
+// Si el elemento es un string debe ser agregado a la lista ‘Strings’.
+// Los elementos deben ser agregados de manera dinámica manipulando el DOM mediante JS.
+
+
+var arr = [1,'dos','tres',4,5,6,'siete',8,9,'diez'];
+var sortList =  function(){
+
+  console.log(typeof arr[0]);
+}
